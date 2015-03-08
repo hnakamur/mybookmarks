91e63f47282d233c5a2544255b4499ef3cb6a400
Hiroaki Nakakmura <hnakamur@gmail.com>
2015-03-08 23:23:41 +0900

    Add Note to Bookmark

--- types.go
+++ types.go
@@ -6,6 +6,7 @@ type Bookmark struct {
 	ID        int       `json:"id"`
 	URL       string    `sql:"type:text" json:"url"`
 	Title     string    `sql:"type:text" json:"title"`
+	Note      string    `sql:"type:text" json:"note"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
